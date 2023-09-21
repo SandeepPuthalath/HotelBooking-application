86908e032f9577d855fc01670b415fa0cb849606
Sandeep <mysisneru@123gmail.com>
2023-09-21 18:07:07 +0530

    signup redirection issue fixed

--- frontend/src/pages/auth/SignupPage.jsx
+++ frontend/src/pages/auth/SignupPage.jsx
@@ -4,25 +4,28 @@ import Loading from "../../components/auth/Loading";
 import ErrorMessage from "../../components/auth/ErrorMessage";
 import { useEffect } from "react";
 import SuccessMessage from "../../components/auth/SuccessMessage";
-import { useNavigate } from "react-router-dom";
 import SignupForm from "../../components/auth/SignupForm";
 import {
   resetSignupError,
   resetSuccesMessage,
 } from "../../redux/reducers/auth/user/userSignupSlice";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const loading = useSelector(s => s.signup?.loading)
-  const successMessage = useSelector(s => s.signup?.successMessage)
-  const error = useSelector(s =>s.signup?.error);
+  const loading = useSelector((s) => s.signup?.loading);
+  const successMessage = useSelector((s) => s.signup?.successMessage);
+  const error = useSelector((s) => s.signup?.error);
 
   const handleErrorClose = () => {
     dispatch(resetSignupError());
   };
 
   const handleSuccesClose = () => {
+    setTimeout(() => {
+      navigate("/login");
+    }, 2000);
     dispatch(resetSuccesMessage());
   };
 
@@ -35,8 +38,20 @@ const SignupPage = () => {
     <div className="relative flex w-screen h-screen justify-center items-center">
       <SignupForm />
       {loading && <Loading />}
-      {error && <ErrorMessage error={error} open={error?true: false} onClose ={handleErrorClose}/>}
-      {successMessage && <SuccessMessage open={successMessage?true: false} message={SuccessMessage} onClose={handleSuccesClose}/>}
+      {error && (
+        <ErrorMessage
+          error={error}
+          open={error ? true : false}
+          onClose={handleErrorClose}
+        />
+      )}
+      {successMessage && (
+        <SuccessMessage
+          open={successMessage ? true : false}
+          message={SuccessMessage}
+          onClose={handleSuccesClose}
+        />
+      )}
     </div>
   );
 };
